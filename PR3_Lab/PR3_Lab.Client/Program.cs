1377c8c5a63b0bf12073975bdce0934dfbb9e55b
MaximBoev <boevmaksim3@gmail.com>
2025-04-10 11:17:41 +0300

    fuck me

--- PR3_Lab/PR3_Lab.Client/Program.cs
+++ PR3_Lab/PR3_Lab.Client/Program.cs
@@ -1,5 +1,17 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PR3_Lab.Client.Pages;
+using PR3_Lab.Components;
+using PR3_Lab.Persistence;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 
 await builder.Build().RunAsync();
